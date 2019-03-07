230befaa1bf2c3618fbcbeb105d0b199fd91a1a2
Ruben Amorim <ruben.amorim@mindera.com>
2019-03-07 20:55:27 +0000

    Add global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Global } from '@emotion/core';
 import styled from '@emotion/styled';
 
 import Head from 'components/Head';
+
+import globalStyles from 'theme/styles/global';
 
 const Title = styled('h1', { shouldForwardProp: (prop) => prop !== 'color' })`
   color: ${(props) => props.color};
@@ -12,6 +15,7 @@ function Homepage(): React.ReactElement {
     return (
       <>
         <Head />
+        <Global styles={ globalStyles } />
         <Title color="red">Hello 👋</Title>
       </>
     );
