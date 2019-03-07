6ff1a3305985e3d0cfe1a23003b9ab63f9d7d945
Ruben Amorim <ruben.amorim@mindera.com>
2019-03-07 20:27:55 +0000

    Add head

---
+++ components/Head.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import NextHead from 'next/head';
+
+function Head(props: Props): React.ReactElement {
+    return (
+        <NextHead>
+            <meta charSet="utf-8" />
+            <title>{ props.title }</title>
+            <meta name="description" content={ props.description } />
+            <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+
+            {/*
+            <link rel="preload" type="font/woff2" as="font" crossOrigin="anonymous" href="/static/fonts/font.woff2" />
+            */}
+
+            <link rel="manifest" crossOrigin="use-credentials" href="/static/manifest.json" />
+            <link rel="shortcut icon" href="/static/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+
+            <meta name="theme-color" content="#fff" />
+        </NextHead>
+    );
+}
+
+Head.defaultProps = {
+    title: 'Ruben Amorim',
+    description: 'Ruben Amorim Personal Page',
+    url: 'test.rubenamorim.dev'
+};
+
+export interface Props {
+    title?: string;
+    description?: string;
+}
+
+export default Head;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import Head from 'components/Head';
+
 const Title = styled('h1', { shouldForwardProp: (prop) => prop !== 'color' })`
   color: ${(props) => props.color};
   text-align: center;
 `;
 
-function Home(): React.ReactElement {
+function Homepage(): React.ReactElement {
     return (
+      <>
+        <Head />
         <Title color="red">Hello 👋</Title>
+      </>
     );
 }
 
-export default Home;
+export default Homepage;
