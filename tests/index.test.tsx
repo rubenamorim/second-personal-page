16c8bb4c6b6f36867bbf9ea42243b2863176f75f
Ruben Amorim <ruben.amorim@mindera.com>
2019-03-18 11:29:21 +0000

    remove tests from build

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 
-import App from '..';
+import App from 'pages';
 
 it('renders the homepage', () => {
     const { getByText } = render(<App />);
